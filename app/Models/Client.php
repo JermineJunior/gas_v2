50a2a7c8d6a2a3dbe5d0f25efb5c75198383deba
Mohamed Ibrahem <obietrice734@gmail.com>
2026-08-29 07:40:51 +0200

    =added clients user

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -12,4 +12,8 @@ class Client extends Model
     {
         return $this->hasMany(Detail::class);
     }
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
